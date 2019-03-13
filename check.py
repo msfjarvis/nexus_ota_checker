4bcd72a85df480d0d255c8547b748ffd917f75d6
Harsh Shandilya <msfjarvis@gmail.com>
2019-03-13 13:53:00 +0530

    Optimize imports and switch to fstrings
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- check.py
+++ check.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from os import makedirs, path
 import re
-import sys
+from os import makedirs, path
 
 import bs4
 import requests
@@ -54,16 +53,16 @@ if __name__ == '__main__':
     makedirs(path.dirname(state_file), exist_ok=True)
 
     soup = bs4.BeautifulSoup(get_page_text(ota_page_url), 'html.parser')
-    latest = soup.find_all('tr', attrs={'id': re.compile('^{}.*'.format(args.name))})
+    latest = soup.find_all('tr', attrs={'id': re.compile(f'^{args.name}.*')})
     if latest and isinstance(latest, list):
         tds = latest[-1].findAll('td')
         version = tds[0].string.strip()
         link = tds[1].find('a').get('href').strip()
         chksum = tds[2].string.strip()
         if args.porcelain:
-            message = '{n},{v},{l},{c}'.format(n=args.name, v=version, l=link, c=chksum)
+            message = f'{args.name},{version},{link},{chksum}'
         else:
-            message = '{n}: {v}\n\n{l}\n\n{c}'.format(n=args.name, v=version, l=link, c=chksum)
+            message = f'{args.name}: {version}\n\n{link}\n\n{chksum}'
 
         current = get_latest_version_state()
         if current:
@@ -73,4 +72,4 @@ if __name__ == '__main__':
             set_latest_version_state(version)
         print(message)
     else:
-        exit('No data found for codename {}. Perhaps a typo or the page layout changed too much?'.format(args.name))
+        exit(f'No data found for codename {args.name}. Perhaps a typo or the page layout changed too much?')
