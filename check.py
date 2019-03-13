4a603d4656fb3edee460135083c61fb4bf89647f
Harsh Shandilya <msfjarvis@gmail.com>
2019-03-13 13:12:00 +0530

    Remove pushbullet support and simplify
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- check.py
+++ check.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
+from os import makedirs, path
 import re
 import sys
 
 import bs4
-import pushbullet
 import requests
 
 ota_page_url = 'https://developers.google.com/android/images'
-pushbullet_token = os.getenv('PUSHBULLET_TOKEN')
 
 
 def get_latest_version_state():
     """
     Get latest version seen from state file
     """
-    if os.path.isfile(state_file):
+    if path.isfile(state_file):
         with open(state_file) as f:
             return f.read()
 
@@ -44,23 +42,14 @@ def get_page_text(url):
         r.raise_for_status()
 
 
-def notify(msg):
-    """
-    Send a message via Pushbullet
-    """
-    if msg:
-        pb = pushbullet.PushBullet(pushbullet_token)
-        pb.push_note('Nexus image update', msg)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-n', '--name', required=True, help="Device codename")
-    parser.add_argument('-f', '--file', default=os.path.join(os.path.expanduser('~'), '.nexus_update_'),
+    parser.add_argument('-f', '--file', default=path.join(path.dirname(path.abspath(__file__)), '.nexus_update_'),
                         help="File to store state in. Device codename is appended automatically.")
     args = parser.parse_args()
     state_file = args.file + args.name
-    os.makedirs(os.path.dirname(state_file), exist_ok=True)
+    makedirs(path.dirname(state_file), exist_ok=True)
 
     soup = bs4.BeautifulSoup(get_page_text(ota_page_url), 'html.parser')
     latest = soup.find_all('tr', attrs={'id': re.compile('^{}.*'.format(args.name))})
@@ -69,18 +58,14 @@ if __name__ == '__main__':
         version = tds[0].string.strip()
         link = tds[1].find('a').get('href').strip()
         chksum = tds[2].string.strip()
+        message = '{n}: {v}\n\n{l}\n\n{c}'.format(n=args.name.title(), v=version, l=link, c=chksum)
 
         current = get_latest_version_state()
         if current:
             if version != current:
                 set_latest_version_state(version)
-                message = '{n}: {v}\n\n{l}\n\n{c}'.format(n=args.name.title(), v=version, l=link, c=chksum)
-                try:
-                    notify(message)
-                except Exception:
-                    print('There is a new OTA, but Pushbullet failed. Here is what I know: {}'.format(message), file=sys.stderr)
-                    raise
         else:
             set_latest_version_state(version)
+        print(message)
     else:
         exit('No data found for codename {}. Perhaps a typo or the page layout changed too much?'.format(args.name))
