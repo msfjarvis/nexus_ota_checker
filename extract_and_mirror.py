e0b129e0cdabd013359c6100eaea47e1df13157b
Harsh Shandilya <msfjarvis@gmail.com>
2019-05-27 17:40:25 +0530

    Use a default device list and break things out of main
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- extract_and_mirror.py
+++ extract_and_mirror.py
@@ -1,5 +1,6 @@
 import argparse
 import hashlib
+import re
 from os import listdir, path, makedirs, rename, remove, removedirs
 from shutil import move, rmtree
 from zipfile import PyZipFile
@@ -7,6 +8,8 @@ from zipfile import PyZipFile
 from pySmartDL import SmartDL
 
 from check import parse
+
+ALL_DEVICES = ['sailfish', 'marlin', 'taimen', 'walleye', 'crosshatch', 'blueline', 'sargo', 'bonito']
 
 
 def sha256_hash(file_path: str) -> str:
@@ -71,29 +74,39 @@ class OtaPackage:
         return path.join(path.dirname(path.abspath(__file__)), f"{self.codename}-{self.release_tag}")
 
 
+def process_packages(args: argparse.Namespace):
+    devices = []
+    regexp = re.compile('[A-Z0-9.]+\d{6}.\d{3}')
+    if not args.name:
+        devices = ALL_DEVICES
+    else:
+        devices.append(args.name)
+    makedirs(args.output, exist_ok=True)
+    for device in devices:
+        raw_data = parse(device, porcelain=True).split("|")
+        device_name = raw_data[0]
+        # This is rather terrible but I'm gonna go with it since atleast some regex is involved
+        release_tag = regexp.search(raw_data[1]).group(0).lower()
+        package_url = raw_data[2]
+        checksum = raw_data[3]
+        otapackage = OtaPackage(device_name, package_url, checksum, release_tag)
+        otapackage.download()
+        otapackage.extract_files()
+        final_dir = path.join(args.output, otapackage.get_output_dir().split("/")[-1])
+        for directory in listdir(args.output):
+            if directory.startswith(device):
+                rmtree(path.join(args.output, directory))
+        move(otapackage.get_output_dir(), final_dir)
+        if args.clean:
+            otapackage.cleanup()
+
+
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-n', '--name', required=True, help="Device codename")
+    parser.add_argument('-n', '--name', required=False, help="Device codename")
     parser.add_argument('-o', '--output', required=True, help="Output directory for extracted images")
     parser.add_argument('-c', '--clean', action="store_true", help="Remove downloaded images once done")
-    args = parser.parse_args()
-    if not path.exists(args.output):
-        makedirs(args.output)
-    raw_data = parse(args.name, porcelain=True).split("|")
-    device_name = raw_data[0]
-    release_tag = raw_data[1].split(",")[0].split("(")[1].lower()  # I can't do py regex for the life of me
-    package_url = raw_data[2]
-    checksum = raw_data[3]
-    otapackage = OtaPackage(device_name, package_url, checksum, release_tag)
-    otapackage.download()
-    otapackage.extract_files()
-    final_dir = path.join(args.output, otapackage.get_output_dir().split("/")[-1])
-    for directory in listdir(args.output):
-        if directory.startswith(args.name):
-            rmtree(path.join(args.output, directory))
-    move(otapackage.get_output_dir(), final_dir)
-    if args.clean:
-        otapackage.cleanup()
+    process_packages(parser.parse_args())
 
 
 if __name__ == '__main__':
