f415080370f76facb9d1f7674bb4b0c528477e59
Harsh Shandilya <me@msfjarvis.dev>
2021-10-29 01:19:07 +0530

    testdata: update

--- tests.py
+++ tests.py
@@ -8,16 +8,16 @@ class Bs4Test(unittest.TestCase):
     def test_parse(self):
         with open("testdata/ota_page.html", "r") as file:
             page_text = file.read()
-        data = parse("walleye", page_text=page_text, porcelain=True).split("|")
+        data = parse("raven", page_text=page_text, porcelain=True).split("|")
         self.assertEqual(4, len(data))
-        self.assertEqual("walleye", data[0])
-        self.assertEqual("qq3a.200705.002", data[1])
+        self.assertEqual("raven", data[0])
+        self.assertEqual("sd1a.210817.015.a4", data[1])
         self.assertEqual(
-            "https://dl.google.com/dl/android/aosp/walleye-qq3a.200705.002-factory-c144ce29.zip",
+            "https://dl.google.com/dl/android/aosp/raven-sd1a.210817.015.a4-factory-bd6cb030.zip",
             data[2],
         )
         self.assertEqual(
-            "c144ce2919f94c4c57a5bd3bb9ae73d242fec52f170e4d1055ac17d0cc884d9e", data[3]
+            "bd6cb030402897f58b952008211640f5241699200d509926540425e00b115bc9", data[3]
         )
 
 
